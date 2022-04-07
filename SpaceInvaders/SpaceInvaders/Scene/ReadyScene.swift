6f7fa4d9ca395b3c082bbf8487d2fab21d4e31b5
ChoiysApple <chcs1370@gmail.com>
2022-04-07 14:14:18 +0900

    Add Text animation

--- SpaceInvaders/SpaceInvaders/Scene/ReadyScene.swift
+++ SpaceInvaders/SpaceInvaders/Scene/ReadyScene.swift
@@ -20,24 +20,32 @@ class ReadyScene: SKScene {
     private func createScoreInfo() {
         
         let spacing: CGFloat = 10.0
+        let duration = 0.5
+        
+        let leftStart: CGFloat = -100.0
+        let rightStart: CGFloat = self.frame.size.width + 100
         
         let scoreTitle = SKLabelNode(fontNamed: kFontName)
         scoreTitle.fontSize = 23
         scoreTitle.fontColor = SKColor.white
         scoreTitle.text = "< SCORE ADVANCE TABLE >"
-        scoreTitle.position = CGPoint(x: self.size.width/2, y: self.size.height*0.8)
+        scoreTitle.position = CGPoint(x: rightStart*2, y: self.size.height*0.8)
         self.addChild(scoreTitle)
+        scoreTitle.run(SKAction.move(to: CGPoint(x: self.size.width/2, y: self.size.height*0.8), duration: duration))
         
         let ufoSprite = SKSpriteNode(imageNamed: "ufo")
         ufoSprite.size = kInvaderSize
         let ufoScore = SKLabelNode(fontNamed: kFontName)
         ufoScore.fontSize = 20
         ufoScore.fontColor = SKColor.white
-        ufoScore.text = "= ? POINTS"
-        ufoScore.position = CGPoint(x: self.size.width/2+ufoSprite.frame.width, y: scoreTitle.position.y - scoreTitle.frame.height - 20)
-        ufoSprite.position = CGPoint(x: ufoScore.position.x - (ufoSprite.frame.width+ufoScore.frame.size.width/2), y: scoreTitle.position.y - scoreTitle.frame.height)
+        ufoScore.text = "= ?? POINTS"
+        ufoScore.position = CGPoint(x: leftStart, y: scoreTitle.position.y - scoreTitle.frame.height - spacing*2)
+        ufoSprite.position = CGPoint(x: leftStart, y: scoreTitle.position.y - scoreTitle.frame.height - spacing*2)
         self.addChild(ufoScore)
         self.addChild(ufoSprite)
+        
+        ufoSprite.run(SKAction.move(to: CGPoint(x: self.size.width/2+ufoSprite.frame.width - (ufoSprite.frame.width+ufoScore.frame.size.width/2), y: scoreTitle.position.y - scoreTitle.frame.height - spacing), duration: duration))
+        ufoScore.run(SKAction.move(to: CGPoint(x: self.size.width/2+ufoSprite.frame.width, y: scoreTitle.position.y - scoreTitle.frame.height - spacing*2), duration: duration))
         
         
         let ASprite = SKSpriteNode(imageNamed: "InvaderA_00")
@@ -46,10 +54,13 @@ class ReadyScene: SKScene {
         AScore.fontSize = 20
         AScore.fontColor = SKColor.white
         AScore.text = "= 10 POINTS"
-        AScore.position = CGPoint(x: self.size.width/2+ASprite.frame.width, y: ufoScore.position.y - ufoScore.frame.height - spacing)
-        ASprite.position = CGPoint(x: AScore.position.x - (ASprite.frame.width+AScore.frame.size.width/2), y: ufoScore.position.y - ufoScore.frame.height)
+        AScore.position = CGPoint(x: rightStart, y: ufoScore.position.y - ufoScore.frame.height - spacing)
+        ASprite.position = CGPoint(x: rightStart, y: ufoScore.position.y - ufoScore.frame.height)
         self.addChild(AScore)
         self.addChild(ASprite)
+        
+        ASprite.run(SKAction.move(to: CGPoint(x: self.size.width/2+ASprite.frame.width - (ASprite.frame.width+AScore.frame.size.width/2), y: ufoScore.position.y - ufoScore.frame.height), duration: duration))
+        AScore.run(SKAction.move(to: CGPoint(x: self.size.width/2+ASprite.frame.width, y: ufoScore.position.y - ufoScore.frame.height - spacing), duration: duration))
         
         let BSprite = SKSpriteNode(imageNamed: "InvaderB_00")
         BSprite.size = kInvaderSize
@@ -57,10 +68,13 @@ class ReadyScene: SKScene {
         BScore.fontSize = 20
         BScore.fontColor = SKColor.white
         BScore.text = "= 10 POINTS"
-        BScore.position = CGPoint(x: self.size.width/2+BSprite.frame.width, y: AScore.position.y - AScore.frame.height - spacing)
-        BSprite.position = CGPoint(x: BScore.position.x - (BSprite.frame.width+BScore.frame.size.width/2), y: AScore.position.y - AScore.frame.height)
+        BScore.position = CGPoint(x: leftStart, y: AScore.position.y - AScore.frame.height - spacing)
+        BSprite.position = CGPoint(x: leftStart, y: AScore.position.y - AScore.frame.height)
         self.addChild(BScore)
         self.addChild(BSprite)
+        
+        BScore.run(SKAction.move(to: CGPoint(x: self.size.width/2+BSprite.frame.width, y: AScore.position.y - AScore.frame.height - spacing), duration: duration))
+        BSprite.run(SKAction.move(to: CGPoint(x: self.size.width/2+BSprite.frame.width - (BSprite.frame.width+BScore.frame.size.width/2), y: AScore.position.y - AScore.frame.height), duration: duration))
         
         let CSprite = SKSpriteNode(imageNamed: "InvaderC_00")
         CSprite.size = kInvaderSize
@@ -68,18 +82,22 @@ class ReadyScene: SKScene {
         CScore.fontSize = 20
         CScore.fontColor = SKColor.white
         CScore.text = "= 10 POINTS"
-        CScore.position = CGPoint(x: self.size.width/2+CSprite.frame.width, y: BScore.position.y - BScore.frame.height - spacing)
-        CSprite.position = CGPoint(x: CScore.position.x - (CSprite.frame.width+CScore.frame.size.width/2), y: BScore.position.y - BScore.frame.height)
+        CScore.position = CGPoint(x: rightStart, y: BScore.position.y - BScore.frame.height - spacing)
+        CSprite.position = CGPoint(x: rightStart, y: BScore.position.y - BScore.frame.height)
         self.addChild(CScore)
         self.addChild(CSprite)
+        
+        CScore.run(SKAction.move(to: CGPoint(x: self.size.width/2+CSprite.frame.width, y: BScore.position.y - BScore.frame.height - spacing), duration: duration))
+        CSprite.run(SKAction.move(to: CGPoint(x: self.size.width/2+CSprite.frame.width - (CSprite.frame.width+CScore.frame.size.width/2), y: BScore.position.y - BScore.frame.height), duration: duration))
 
         let startLabel = SKLabelNode(fontNamed: kFontName)
         startLabel.fontSize = 20
         startLabel.fontColor = SKColor.green
         startLabel.text = "TAP ANYWHERE TO START"
-        startLabel.position = CGPoint(x: self.size.width/2, y: CScore.position.y - CScore.frame.height - spacing*5)
+        startLabel.position = CGPoint(x: leftStart*2, y: CScore.position.y - CScore.frame.height - spacing*5)
         addChild(startLabel)
 
+        startLabel.run(SKAction.move(to: CGPoint(x: self.size.width/2, y: CScore.position.y - CScore.frame.height - spacing*5), duration: duration))
         
 
     }
