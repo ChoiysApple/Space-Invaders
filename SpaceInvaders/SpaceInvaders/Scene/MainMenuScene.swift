133b863319985edd7282b37f521aa1d3145855b9
ChoiysApple <chcs1370@gmail.com>
2022-04-07 10:56:48 +0900

    Create ReadyScene

--- SpaceInvaders/SpaceInvaders/Scene/MainMenuScene.swift
+++ SpaceInvaders/SpaceInvaders/Scene/MainMenuScene.swift
@@ -10,19 +10,19 @@ import SpriteKit
 class MainMenuScene: SKScene {
     
     override func didMove(to view: SKView) {
+        
         createContent()
         run(SKAction.playSoundFileNamed("main.wav", waitForCompletion: false))
     }
 
     
     func createContent() {
+        let moveActionDuration = 2.0
                 
         let logoImageView = SKSpriteNode(imageNamed: "Logo")
         logoImageView.size = CGSize(width: self.size.width/1.5, height: logoImageView.size.height)
         logoImageView.position = CGPoint(x: self.size.width/2, y: self.size.height + logoImageView.size.height)
         self.addChild(logoImageView)
-        
-
         
         let tapLabel = SKLabelNode(fontNamed: kFontName)
         tapLabel.fontSize = 20
@@ -30,27 +30,27 @@ class MainMenuScene: SKScene {
         tapLabel.text = "< Tap Anywhere >"
         tapLabel.position = CGPoint(x: self.size.width/2, y: -tapLabel.frame.size.height)
         self.addChild(tapLabel)
+
+        
         
         // black space color
         self.backgroundColor = SKColor.black
         
         
-        let logoAction = SKAction.move(to: CGPoint(x: self.size.width/2, y: self.size.height/1.5), duration: 2.0)
-        let labelAction = SKAction.move(to: CGPoint(x: self.size.width/2, y: self.size.height/1.5 - logoImageView.frame.size.height - 40), duration: 2.0)
+        let logoAction = SKAction.move(to: CGPoint(x: self.size.width/2, y: self.size.height/1.5), duration: moveActionDuration)
+        let labelAction = SKAction.move(to: CGPoint(x: self.size.width/2, y: self.size.height/1.5 - logoImageView.frame.size.height - 40), duration: moveActionDuration)
         
         logoImageView.run(logoAction)
         tapLabel.run(labelAction)
-
-
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)  {
         
-        let gameScene = GameScene(size: self.size)
-        gameScene.scaleMode = .aspectFill
+        let readyScene = ReadyScene(size: self.size)
+        readyScene.scaleMode = .aspectFill
         
-        self.view?.presentScene(gameScene, transition: SKTransition.doorsCloseHorizontal(withDuration: 1.0))
-        
+        self.view?.presentScene(readyScene, transition: SKTransition.doorsCloseHorizontal(withDuration: 1.0))
     }
 
 }
--- SpaceInvaders/SpaceInvaders/Scene/ReadyScene.swift
+++ SpaceInvaders/SpaceInvaders/Scene/ReadyScene.swift
@@ -2,21 +2,60 @@
 //  ReadyScene.swift
 //  SpaceInvaders
 //
-//  Created by Daegeon Choi on 2022/04/06.
+//  Created by Daegeon Choi on 2022/04/07.
 //
 
 import SpriteKit
 
 class ReadyScene: SKScene {
-
+    
     override func didMove(to view: SKView) {
-        createContent()
+        
+        self.backgroundColor = .black
+        
+        
+        createControlGuide()
     }
     
-    private func createContent() {
+    private func crateScoreInfo() {
         
-
     }
+
+    private func createControlGuide() {
+        
+        let guidePath = CGMutablePath()
+        guidePath.addRect(CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: self.size.width, height: self.size.width/3)))
+        
+        let guidePanel = SKShapeNode(path: guidePath)
+        guidePanel.strokeColor = .green
+        guidePanel.lineWidth = 1
+        guidePanel.glowWidth = 1
+        self.addChild(guidePanel)
+        
+        let seperatorL = SKSpriteNode(color: .green, size: CGSize(width: 1, height: self.size.width/3))
+        let seperatorR = SKSpriteNode(color: .green, size: CGSize(width: 1, height: self.size.width/3))
+        seperatorL.position = CGPoint(x: self.size.width*kShipRelativeControlSize, y: self.size.width/6)
+        seperatorR.position = CGPoint(x: self.size.width*(1-kShipRelativeControlSize), y: self.size.width/6)
+        self.addChild(seperatorR)
+        self.addChild(seperatorL)
+        
+        let leftLabel = SKLabelNode(fontNamed: kFontName)
+        leftLabel.fontSize = 50
+        leftLabel.fontColor = SKColor.white
+        leftLabel.text = "<"
+        leftLabel.position = CGPoint(x: self.size.width*kShipRelativeControlSize/2, y: (guidePanel.frame.size.height-leftLabel.fontSize)/2)
+        self.addChild(leftLabel)
+        
+        let rightLabel = SKLabelNode(fontNamed: kFontName)
+        rightLabel.fontSize = 50
+        rightLabel.fontColor = SKColor.white
+        rightLabel.text = ">"
+        rightLabel.position = CGPoint(x: self.size.width*(1-kShipRelativeControlSize/2), y: (guidePanel.frame.size.height-rightLabel.fontSize)/2)
+        self.addChild(rightLabel)
+
+        
+    }
+    
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)  {
         
@@ -25,5 +64,4 @@ class ReadyScene: SKScene {
         
         self.view?.presentScene(gameScene, transition: SKTransition.doorsCloseHorizontal(withDuration: 1.0))
     }
-
 }
